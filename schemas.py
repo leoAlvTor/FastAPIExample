6cc190607b200f8c8e03b217859b57af7fbf06a6
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-04-02 21:22:58 -0500

    What if a row has an empty column value? Using optional for values with null values.

--- schemas.py
+++ schemas.py
@@ -1,27 +1,29 @@
 from datetime import date
+from typing import Optional
+
 from pydantic import BaseModel
 
 
 class Product(BaseModel):
-    CODIGO = str
-    CANTIDADINTERNA = int
-    CATEGORIA = str
-    CODIGOPRINCIPAL = str
-    FECHA = date
-    IMPUESTO = str
-    NOMBRE = str
-    NOMBREPRINCIPAL = str
-    PRECIOCAJA = float
-    PRECIOCAJA12 = float
-    PRECIOUNITARIO = float
-    PRECIOUNITARIO12 = float
-    PRECIOVENTACAJA = float
-    PRECIOVENTAUNIDAD = float
-    REPRESENTACION = str
-    STOCKPAQUETE = float
-    STOCKUNIDAD = float
-    PROVEEDOR_RUC = str
-    PROMOCION = str
+    CODIGO: str
+    CANTIDADINTERNA: int
+    CATEGORIA: str
+    CODIGOPRINCIPAL: str
+    FECHA: date
+    IMPUESTO: str
+    NOMBRE: str
+    NOMBREPRINCIPAL: str
+    PRECIOCAJA: float
+    PRECIOCAJA12: float
+    PRECIOUNITARIO: float
+    PRECIOUNITARIO12: float
+    PRECIOVENTACAJA: float
+    PRECIOVENTAUNIDAD: float
+    REPRESENTACION: str
+    STOCKPAQUETE: float
+    STOCKUNIDAD: Optional[float] = 0
+    PROVEEDOR_RUC: str
+    PROMOCION: Optional[str] = 'NA'
 
     class Config:
         orm_mode = True
