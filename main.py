d83e6cdda345d3a707a136d666d4953561620836
leoAlvTor <calvaradot1@est.ups.edu.ec>
2021-04-02 21:24:10 -0500

    Done with FastAPI first steps, getting all products from database and returning them as a JSON.

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
+from starlette.responses import RedirectResponse
+
+import models, schemas
+from database import SessionLocal
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+    allow_credentials=True,
+)
+
+def get_database():
+    global db
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
 
 @app.get('/')
-def index():
-    return 'Hey from FastAPI'
+def main():
+    return RedirectResponse(url='/docs/')
 
 
-@app.get('/name')
-def get_name():
-    return {'Name': 'Leo Alvarado!'}
+@app.get('/products/', response_model=List[schemas.Product])
+def get_products(database: Session = Depends(get_database)):
+    productos = database.query(models.Producto).all()
+    return productos
